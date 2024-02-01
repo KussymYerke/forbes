8b2ce91a1dd112ead26d93110afcaf4c2ec61f43
KussymYerke <e_kusym@kbtu.kz>
2024-02-01 23:04:51 +0600

    upd2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,26 +7,26 @@ const Contact = () => {
   return (
     <div className="contact-page-wrapper">
       <div className="articles">
-          <div className="article">
-              <a href="https://forbes.kz/woman/forbes_woman_club_priglasil_marian_zaltsman/">
-                <img src={Article1} width={"100%"} height={"33%"} alt="article"/>
-              </a>
-              <div className="article-title">Forbes Woman Club 2021</div>
-              <a className="article-button" href="https://forbes.kz/woman/forbes_woman_club_priglasil_marian_zaltsman/">Прочитать статью</a>
+      <div className="wrapper">
+        <div className="image">
+          <img src={Article1} width={"100%"} height={"33%"} alt="article"/>
+          <div className="content">
+            <div className="article-info">
+              <div className="content-title">Forbes Woman Club 2021</div>
+              <a className="content-button" href="https://forbes.kz/woman/forbes_woman_club_priglasil_marian_zaltsman/">Прочитать статью</a>
+            </div>
           </div>
-        <div className="article">
-            <a href="https://forbes.kz/woman/forbes_woman_club_2022_kak_nayti_sebya_v_biznese/">
-              <img src={Article2} width={"100%"} height={"33%"} alt="article" />
-              </a>
-            <div className="article-title">Forbes Woman Club 2022</div>
-            <a className="article-button" href="https://forbes.kz/woman/forbes_woman_club_2022_kak_nayti_sebya_v_biznese/">Прочитать статью</a>
         </div>
-        <div className="article">
-            <a href="https://forbes.kz/woman/tsifrovizatsiya_i_liderstvo_chto_obsujdali_na_forbes_woman_club_2023/">
-             <img src={Article3} width={"100%"} height={"33%"} alt="article"/>
-            </a>
-            <div className="article-title">Forbes Woman Club 2023</div>
-            <a className="article-button" href="#https://forbes.kz/woman/tsifrovizatsiya_i_liderstvo_chto_obsujdali_na_forbes_woman_club_2023/">Прочитать статью</a>
+      </div>
+      <div className="wrapper">
+        <div className="image">
+          <img src={Article2} width={"100%"} height={"33%"} alt="article"/>
+          <div className="content">
+            <div className="article-info">
+              <div className="content-title">Forbes Woman Club 2022</div>
+              <a className="content-button" href="https://forbes.kz/woman/forbes_woman_club_2022_kak_nayti_sebya_v_biznese/">Прочитать статью</a>
+            </div>
+          </div>
         </div>
       </div>
       <div className="wrapper">
@@ -39,6 +39,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
+      </div>
       </div>
       <div className="videos">
         <iframe className="video"  src="https://www.youtube.com/embed/UDZAllm970c?si=JOOtgt-yhSLMxFSq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
