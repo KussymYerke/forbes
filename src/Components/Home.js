78f5f3e218174be0f1139216acf98342db7b3554
KussymYerke <e_kusym@kbtu.kz>
2024-02-02 12:21:26 +0600

    glob

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,4 @@
 import React from "react";
-import BannerBackground from "../Assets/home-banner-background.png";
-import BannerImage from "../Assets/home-banner-image.png";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 import "slick-carousel/slick/slick.css"; 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,6 +17,7 @@ import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
 import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
 import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 import File from '../Assets/ForbesFile.pdf'
+import { Viewer } from '@react-pdf-viewer/core';
 
 
 const Navbar = () => {
@@ -53,12 +54,42 @@ const Navbar = () => {
       id: "#team"
     }
   ];
+
+  const [shown, setShown] = useState(false);
+
+  const modalBody = () => (
+    <div
+        style={{
+            backgroundColor: '#fff',
+
+            /* Fixed position */
+            left: 0,
+            position: 'fixed',
+            top: 0,
+
+            /* Take full size */
+            height: '100%',
+            width: '100%',
+
+            /* Displayed on top of other elements */
+            zIndex: 9999,
+
+            display: 'flex',
+            flexDirection: 'column',
+            overflow: 'hidden',
+        }}
+    >
+        <Viewer fileUrl={File} />
+    </div>
+);
+
   return (
     <nav>
       <div className="nav">
         <div className="nav-inner">
             <img className="logo" src={Logo} alt="" />
             <div className="navbar-links-container">
+            <button onClick={() => setShown(true)}>Open modal</button>
               <a href={File} target="blank">MediaKit</a>
               <a href="#hero">Forbes Woman Club</a>
               <a href="#priveleges">Привилегии</a>
