9a9d2cca72e6d0e19d4fc4642acdc292926bd0cb
KussymYerke <e_kusym@kbtu.kz>
2024-02-03 20:50:25 +0600

    folder

--- src/App.js
+++ src/App.js
@@ -2,33 +2,22 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
-import Testimonial from "./Components/Testimonial";
-import Contact from "./Components/Contact";
+import Privelleges from "./Components/Privelleges";
 import Footer from "./Components/Footer";
 import Events from "./Components/Events";
+import Articles from "./Components/Articles";
 import Member from "./Components/Member";
-import { Helmet } from 'react-helmet';
 
 function App() {
-
-  
   return (
     <div className="App">
-      <Helmet>
-        <title>My Page Title</title>
-        <meta name="description" content="ForbesWoman" />
-        <meta name="keywords" content="forbes, forbeswoman, woman" />
-        <meta name="author" content="Forbes" />
-        <meta property="og:title" content="ForbesWoman" />
-        <meta property="og:description" content="ForbesWoman" />
-      </Helmet>
       <Home />
       <div className="container">
         <About />
         <Member/>
         <Work />
-        <Testimonial />
-        <Contact />
+        <Privelleges />
+        <Articles />
         <Events />
       </div>
       <Footer />
